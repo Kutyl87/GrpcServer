2cf0f98303447d77d0ed4551d48e715d1bc56b98
Michal <michal.ciupak0@gmail.com>
2024-01-21 00:12:39 +0200

    number id

---
+++ Migrations/20240120220900_migration9.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryGrpc.Migrations
+{
+    /// <inheritdoc />
+    public partial class migration9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumberId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberId",
+                table: "AspNetUsers");
+        }
+    }
+}
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -7,6 +7,7 @@ public class Customer : IdentityUser
     // [Key]
     // public int CustomerId { get; set; }
 
+    public int NumberId { get; set; }
     public string Name { get; set; } = null!;
 
     public string Surname { get; set; } = null!;
